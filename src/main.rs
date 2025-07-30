a3618fe094a7f67ae3afb68ef0694ef979b4a903
SpiritSeal <yssaketh@gmail.com>
2025-07-29 20:43:25 -0400

    basic stats tracking

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,61 @@
-#[cfg(feature = "unstable_grab")]
-use rdev::{grab, Event};
 
-#[cfg(feature = "unstable_grab")]
-fn callback(event: Event) -> Option<Event> {
-    println!("Event: {:?}", event);
-    Some(event) // Return None to suppress the event
-}
 
-fn main() {
-    #[cfg(feature = "unstable_grab")]
-    if let Err(error) = grab(callback) {
-        println!("Error: {:?}", error);
+
+use evdev::*;
+use std::thread;
+use std::time::Duration;
+use std::collections::HashMap;
+
+
+fn monitor_loop() -> Result<(), Box<dyn std::error::Error>> {
+
+    // let handle = thread::spawn(|| {
+    //     println!("test");
+    // });
+    
+    let mut device = Device::open("/dev/input/event5")?;
+    let mut total_cnt = 0;
+    let mut uniq = 0.0;
+    let mut key_stats = HashMap::new();
+    
+    loop {
+        for event in device.fetch_events().unwrap(){
+            if let EventSummary::Key(ev, kc, state) = event.destructure() {
+                // println!("Count {}\t{:?}", cnt, &kc);
+                // check if released
+                if state != 1 {
+                    // println!("Skip {ev:?}");
+                    continue;
+                }
+                // println!("{ev:?}");
+                if let Some(item_count) = key_stats.get_mut(&kc) {
+                    *item_count += 1;
+                    total_cnt += 1;
+                } else {
+                    key_stats.insert(kc, 1);
+                }
+            }
+        }
+
+        thread::sleep(Duration::from_millis(1000));
+
+        let len = key_stats.len();
+        if len == 0 {
+            println!("No keys pressed");
+            continue;
+        }
+
+        println!("{key_stats:?}");
+        println!("Count {}", total_cnt);
+        uniq = total_cnt as f32 / key_stats.len() as f32;
+        println!("Uniq {}, len {}", uniq, len);
+
+        // Clear key stats
+        key_stats.clear();
     }
-    println!("Done");
+    // handle.join().unwrap();
 }
-
+fn main() {
+    monitor_loop();
+}
 
