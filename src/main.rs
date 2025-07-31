0dd8223efee96185f1bbba4cae42e289fd3601a0
SpiritSeal <yssaketh@gmail.com>
2025-07-31 10:03:33 -0700

    some cleanup

--- src/main.rs
+++ src/main.rs
@@ -29,61 +29,37 @@ enum TrackableEvent {
 fn monitor_loop(m: Arc::<Mutex::<HashMap::<TrackableEvent, i32>>>) {   
     loop {
         thread::sleep(time::Duration::from_secs(1));
-        let hm = m.lock().unwrap();
+        let mut hm = m.lock().unwrap();
         dbg!(&hm);
+        hm.clear();
     }
 }
 
-fn keyboard_listener(m_clone2: Arc<Mutex<HashMap<TrackableEvent, i32>>>, tx: Sender<TrackableEvent>) -> impl Fn() {
+fn keyboard_listener(tx: Sender<TrackableEvent>) -> impl Fn() {
     move || {
-        let m = m_clone2.clone();
-        if let Err(error) = listen(callback_generator(m, tx.clone())) {
+        if let Err(error) = listen(callback_generator(tx.clone())) {
             println!("Error: {:?}", error);
         };
     }
 }
 
 
-fn callback_generator(m: Arc::<Mutex::<HashMap::<TrackableEvent, i32>>>, tx: Sender<TrackableEvent>) -> impl Fn(Event) {
+fn callback_generator(tx: Sender<TrackableEvent>) -> impl Fn(Event) {
    
     move |event: Event| {
-        // println!("My callback {:?}", event);
-        // match event.name {
-            //     Some(string) => println!("User wrote {:?}", string),
-            //     None => (),
-            // }
-            // println!("{event:?}");
-            // match event.event_type {
-                //     EventType::ButtonPress(p) => println!("{p:?} do"),
-                //     EventType::KeyPress(p) => println!("{p:?} do"),
-                //     EventType::ButtonRelease(p) => println!("{p:?} up"),
-                //     EventType::KeyRelease(p) => println!("{p:?} up"),
-                //     _ => (),
-                // }
         let tracked = match event.event_type {
             EventType::ButtonPress(b) => Some(TrackableEvent::ButtonPress(InputKey::Mouse(b))),
             EventType::KeyPress(k) => Some(TrackableEvent::KeyPress(InputKey::Keyboard(k))),
             _ => None,
         };
-        // if let Some(tr) = tracked {
-            // let mut hm = m.lock().unwrap();
-            // if let Some(v) = hm.get_mut(&tr) {
-                // println!();
-                // *v += 1
-            // } else {
-                // hm.insert(tr, 1);
-            // };
-        // };
-        // if let Some(tr) = tracked {
-        //     let mut map = m.lock().unwrap();
-        //     *map.entry(tr).or_insert(0) += 1;
-        // };
         if let Some(tr) = tracked {
-            tx.send(tr);
+            match tx.send(tr) {
+                Ok(_) => (),
+                Err(e) => {dbg!(e);},
+            };
         };
 
     }
-
 }
 
 fn signal_consumer(m: Arc<Mutex<HashMap<TrackableEvent, i32>>>, rx: Receiver<TrackableEvent>) {
@@ -92,7 +68,6 @@ fn signal_consumer(m: Arc<Mutex<HashMap<TrackableEvent, i32>>>, rx: Receiver<Tra
         let m_clone = m.clone();
         let mut hm = m_clone.lock().unwrap();
         *hm.entry(tr).or_insert(0) += 1;
-        // dbg!(&hm);
     }
 }
 
@@ -104,8 +79,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let (tx, rx) = channel();
 
-    let m_clone2 = Arc::clone(&m);
-    let handle2 = thread::spawn(keyboard_listener(m_clone2, tx.clone()));
+    // let m_clone2 = Arc::clone(&m);
+    let handle2 = thread::spawn(keyboard_listener(tx.clone()));
     
     let m_clone3 = Arc::clone(&m);
     let handle3 = thread::spawn(move || signal_consumer(m_clone3, rx));
